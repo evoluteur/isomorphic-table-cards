bb3421267798a9dcf8d4b21cc1962cfa095a34f7
evoluteur <ogiulieri@gmail.com>
2020-10-05 22:03:40 -0700

    Config options.

--- js/isomorphic-table-cards.js
+++ js/isomorphic-table-cards.js
@@ -1,95 +1,65 @@
 // https://github.com/evoluteur/isomorphic-table-cards
 // (c) 2020 Olivier Giulieri
 
-const layoutInfo = {
-	table: {
-		// ---- row position & size
-		top: i => 40+i*31+'px',
-		left: i => 0,
-		// ---- table header (hidden)
-		headerLeft: 0,
-	},
-	cards: {
-		// ---- card position & size
-		top: i => Math.floor(i/cardsPerRow)*94+'px',
-		left: i => (i%cardsPerRow)*210+'px',
-		// ---- table header (hidden)
-		headerLeft: '-650px',
-	}
-};
-
-const selector = '.holder';
 let cardsPerRow = 3;
 class IsomorphicTableCards {
 	
+	selector = '.holder';
 	holder = null;
 	curSortField = 'chakra';
 	curSortDirection = 1;
 	curStyle = 'cards'; // "table" or "cards"
 
-	getLayoutInfo(style){
-		cardsPerRow = Math.floor((window.innerWidth -40)/210);
-		return layoutInfo[style];
+	constructor(config){
+		this.config = config
 	}
 	
 	render(){
-		const l = this.getLayoutInfo(this.curStyle);
-		this.holder = document.querySelector(selector);
+		this.holder = document.querySelector(this.selector);
 		this.node = document.createElement("div");
-		this.node.className = 'cards'
-		this.node.innerHTML = data.map(d => `<div class="item chakra${d.chakra}" id="${d.name}">
-				<div class="c1">
-					${d.name}
-				</div>
-				<div class="c2">
-					${d.spirit}
-				</div>
-			</div>
-		`).join('')
+		this.node.className = this.curStyle
+		this.node.innerHTML = data.map(this.config.itemTemplate).join('')
 		this.holder.appendChild(this.node)
-		this.layout(true, false);
+		this.layout(false);
 	}
 	
 	redraw(style){
-		this.node.className = style
+		if(style){
+			this.node.className = style
+		}
 		this.curStyle = style || this.curStyle;
 		this.layout();
 	}
 	
 	sort(key){
 		const csDirection = key===this.curSortField ? -this.curSortDirection : 1;
-		  
-		this.getLayoutInfo(this.curStyle);
-		if(key=='chakra'){
-			data.sort(csDirection>0 ? 
-				(a, b) => (a.chakra+a.name).localeCompare(b.chakra+b.name)
-				: 
-				(a, b) => ((8-a.chakra)+a.name).localeCompare((8-b.chakra)+b.name)
-			)
-		}else{
-			data.sort((a, b) => csDirection*a.name.localeCompare(b.name))
-		}
-		this.curSortField=key;
-		this.layout(false, true);
+		this.curSortDirection = csDirection
+		data = this.config.sort(data, key, csDirection)
+		this.curSortField = key;
+		this.layout(true);
 	}
 	
-	layout(skipAnim, skipChildren){
-		const l = this.getLayoutInfo(this.curStyle)
-	
-		const id2idx ={}
+	layout(keepStyle){
+		const isCards = this.curStyle === 'cards'
+		const cardsPerRow = Math.floor((window.innerWidth -40)/this.config.cardWidth);
+		const fnTop = isCards ? idx => Math.floor(idx/cardsPerRow)*this.config.cardHeight+'px'
+			: idx => 40+idx*this.config.rowHeight+'px'
+		const fnLeft = isCards ? idx => (idx%cardsPerRow)*this.config.cardWidth+'px'
+			: idx => 0
+		const id2idx = {}
 		data.forEach((d, idx) => id2idx[d.name] = idx)
 	 
 		this.holder.querySelectorAll('.item')
 			.forEach(e => {
 				const idx = id2idx[e.id]
-				e.style = 'left:'+l.left(idx)+';top:'+l.top(idx)
+				e.style = 'left:'+fnLeft(idx)+';top:'+fnTop(idx)
 			})
 	
-		if(!skipChildren){
-			this.holder.querySelector('.header').style = 'left:'+l.headerLeft;
+		if(!keepStyle){
+			this.holder.querySelector('.header').style = 'left:'+(this.curStyle==='cards' ? '-650px' : 0);
 			const totalHeight = 20+(this.curStyle==='cards' ?
-					Math.ceil(data.length/cardsPerRow)*94
-					 : 40+data.length*31);
+					Math.ceil(data.length/cardsPerRow)*this.config.cardHeight
+					 : 40+data.length*this.config.rowHeight);
 			this.holder.style.height = totalHeight+'px'
 		}
 		
